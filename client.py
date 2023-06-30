485b223aa7410387f154f34af497ed18a80f7b2c
ImMamey <32584037+ImMamey@users.noreply.github.com>
2023-06-30 13:13:05 -0400

    [fix] Eliminar programación asincrona

--- client.py
+++ client.py
@@ -1,42 +1,82 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
+import socket
+import re
+import sys
 
-import asyncio
+class Client:
+    """
+    Contiene todos los metodos para el cliente.
+    """
+    def __init__(self,ip) -> None:
+        """
+        :var HEADER: Numero de bytes usado para algoritmo logico = como no sabemos cual es el tamaño de cada mensaje,
+        todos los mensajes seran de 64 bytes. Facilita el encode/decode.
+        :var PORT: Numero de puerto a usar.
+        :var FORMAT: Formato de encode.
+        :var DISCONNECT_MESSAGE: mensaje para la desconneción y cierre de sesion.
+        :var SERVER: IP del servidor.
+        :var ADDR: tupla con el IP del servidor y puerto del socket.
+
+        """
+        self.HEADER: int = 64
+        self.PORT: int = 5555
+        self.FORMAT: str = 'utf-8'
+        self.DISCONNECT_MESSAGE: str = "!DISCONNECT"
+        self.SERVER: str = ip #"192.168.56.1"
+        self.ADDR: tuple = (self.SERVER, self.PORT)
+
+        self.start()
+
+    def start(self)->None:
+        """
+        Inicia el socket del cliente, se conectara a la tupla con los datos del servidor
+        :return: None
+        """
+        try:
+            self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            self.client.connect(self.ADDR)
+        except Exception as e:
+            exception: str = f"{type(e).__name__}: (e)"
+            print(f"Error al tratar de conectarse al servidor: \n{exception}")
+            sys.exit("El cliente se cerrará despues de el log....")
 
 
-async def init_socket (loop):
-    reader, writer = await asyncio.open_connection('192.168.1.101', 5555, loop=loop)
-    return reader, writer
-
-
-async def tcp_ping(writer, loop):
-    while True:
-        print('Send: ping')
-        writer.write("ping".encode())
-        print("ping sent")
-        await asyncio.sleep(3)
-        print("sleep finished")
-
-
-async def tcp_read(reader, loop):
-    while True:
-        data = await reader.read(100)
-        print('Received: %r' % data.decode())
-
-
-def main():
-    reader = None
-    writer = None
-    loop = asyncio.get_event_loop()
-    reader, writer = loop.run_until_complete(init_socket(loop))
-    tasks = [
-        tcp_read(reader, loop),
-        tcp_ping(writer, loop),
-    ]
-    loop.run_until_complete(asyncio.wait(tasks))
-    loop.close()
-    writer.close()
+    def send(self, msg) -> None:
+        """
+        Trata de enviar un mensaje al servidor. Si no devuelve un error.
+        :param msg: Mensaje a enviar al servidor
+        :return: None
+        """
+        try:
+            message = msg.encode(self.FORMAT)  # encode el mensaje con el formato utf-8
+            msg_length = len(message)  # calcula cantidad de caracteres en el mensaje
+            send_length = str(msg_length).encode(self.FORMAT) #chequea que los mensajes sean de 64bytes
+            send_length += b' ' * (self.HEADER - len(send_length))
+            self.client.send(send_length) #se envia los mensajes
+            self.client.send(message)
+            print(self.client.recv(2048).decode(self.FORMAT))
+        except Exception as e:
+            exception: str = f"{type(e).__name__}: (e)"
+            print(f"Error al enviar el mensaje: \n{exception}")
 
 
 if __name__ == "__main__":
-    main()
+    detente: bool = False
+
+    while not detente:
+        ip: str= input("Escriba el ip del servidor:\n")
+        pattern = re.compile("^(?:[0-9]{1,3}\.){3}[0-9]{1,3}$")
+        if not pattern.match(ip):
+            print("La ip no es válida. Intente nuevamente: \n")
+        else:
+            detente = True
+
+    cl = Client(ip)
+    detente: bool = False
+
+    while not detente:
+        msg: str = input("Escriba un mensaje para enviar al Servidor (escriba \"n\" para detener)\n")
+        if msg=="n":
+            detente=True
+            cl.send(cl.DISCONNECT_MESSAGE)
+        else:
+            cl.send(str(msg))
--- server.py
+++ server.py
@@ -1,31 +1,88 @@
-import asyncio
-import sys
 import socket
+import threading
 import logging
 
 LOG = logging.getLogger("server")
 
 clients = dict()
 
+class Server:
+    """
+    Contiene todos los metodos necesarios para inciar un servidor usando Hilos.
+    """
+    class Network:
+        """
+        Contiene los atributos del servidor para facil acceso en una subclase anidada. Tambien crea el socket principal
+        """
+        def __init__(self)-> None:
+            """
+            :var SERVER: Guarda la IP del servidor
+            :var PORT: Numero de puerto a usar
+            :var ADDR: Tupla con los datos de: ip y numero de puerto
+            :var FORMAT: Formato de encode
+            :var DISCONNECT_MESSAGE: mensaje para la desconección y cierre de sesion.
+            :var HEADER: Numero de bytes usado para algoritmo logico = como no sabemos cual es el tamaño de cada mensaje, todos los mensajes seran de 64 bytes. Facilita el encode/decode.
+            :var server: socket del servidor.
+            """
+            # obtener lan ip
+            ips = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+            ips.connect(("8.8.8.8", 80))
 
-def client_connected_cb(client_reader, client_writer) -> None:
-    """Inicia el servidor"""
-    client_id = client_writer.get_extra_info("peername")
-    LOG.info("Client connected: {}".format(client_id))
 
+            self.SERVER: str = ips.getsockname()[0]
+            #cerrar  lan ips
+            ips.close()
 
-async def main() -> None:
-    server = await asyncio.start_server(client_connected_cb, host=host, port=port)
+            self.PORT: int = 5555 #purto a usar
+            self.ADDR: tuple = (self.SERVER, self.PORT)
+            self.FORMAT: str = 'utf-8'
+            self.DISCONNECT_MESSAGE: str= "!DISCONNECT"
+            self.HEADER: int =64
 
-    addrs = ", ".join(str(sock.getsockname()) for sock in server.sockets)
-    LOG.info(f"Serving on:{addrs}")
+            self.server: socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            self.server.bind(self.ADDR)
 
-    try:
-        async with server:
-            await server.serve_forever()
-    except Exception as e:
-        exception: str = f"{type(e).__name__}: (e)"
-        LOG.exception("Fallo al correr la corutina del servidor")
+    def handle_client(self, conn, addr,n):
+        """
+        Metodo que maneja clientes, recibe mensajes y envia mensajes de confirmacion
+        :param conn: connecion del socket.
+        :param addr: tupla (ip,socket)
+        :param n: Instancia de clase anidada "network"
+        :return: None
+        """
+        print(f"[NUEVA CONNECCION] {addr} connectado.")
+
+        connected: bool = True
+        while connected:
+            msg_length= conn.recv(n.HEADER).decode(n.FORMAT)
+            if msg_length:
+                msg_length = int(msg_length)
+                msg = conn.recv(msg_length).decode(n.FORMAT)
+                if msg == n.DISCONNECT_MESSAGE:
+                    connected = False
+                print(f"[{addr}] {msg}")
+                conn.send("[SERVIDOR] Mensaje recibido.".encode(n.FORMAT))
+
+    def start(self):
+        """
+        Incia el servidor mediante un try and catch.
+        :return:  None
+        """
+        try:
+            n = self.Network()
+            n.server.listen()
+            LOG.info("El servidor esta activo en la IP: %s",n.SERVER)
+            LOG.info("[Escuchando] Servidor esta escuchando...")
+            while True:
+                conn, addr = n.server.accept()
+                thread = threading.Thread(target=self.handle_client, args=(conn, addr, n))
+                thread.start()
+                LOG.info(f"[CONNECIONES ACTIVAS] {threading.active_count() - 1}")
+
+        except Exception as e:
+            exception: str = f"{type(e).__name__}: (e)"
+            print(f"Error al Iniciar el servidor. El error fue: \n{exception}")
+
 
 
 if __name__ == "__main__":
@@ -37,18 +94,7 @@ if __name__ == "__main__":
         handlers=[logging.StreamHandler(), logging.FileHandler("stuff.log")],
     )
 
-    try:
-        ips = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        ips.connect(("8.8.8.8", 80))
-        host: str = ips.getsockname()[0]
-        ips.close()
-    except Exception as e:
-        exception: str = f"{type(e).__name__}: (e)"
-        LOG.exception(
-                "Fallo al cargar la IP publica del servidor\n"
-                "Se montará el servidor en localhost: 127.0.0.1"
-        )
-        host: str = "127.0.0.1"
-
-    port = 5555
-    asyncio.run(main())
+    if __name__ == "__main__":
+        LOG.info("[INICIANDO SERVIDOR] el servidor está iniciando....")
+        s = Server()
+        s.start()
